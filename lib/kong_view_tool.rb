1478c60e95bc249b6ed9939baa95e64c9debf6c2
kongou <oukong84@gmail.com>
2020-01-20 17:25:47 +0700

    first commit

---
+++ lib/kong_view_tool.rb
@@ -0,0 +1,6 @@
+require "kong_view_tool/version"
+
+module KongViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kong_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KongViewTool
+  VERSION = "0.1.0"
+end
